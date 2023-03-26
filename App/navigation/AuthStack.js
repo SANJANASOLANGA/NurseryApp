8f2326b49868cf7d64e812069a9a629141112db9
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-03-26 16:59:03 +0530

    Added google-sign-in

--- App/navigation/AuthStack.js
+++ App/navigation/AuthStack.js
@@ -2,8 +2,8 @@ import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import OnboardingScreen from '../screens/OnboardingScreen';
@@ -16,28 +16,28 @@ const AuthStack = () => {
   const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
   let routeName;
 
-  // useEffect(() => {
-  //   AsyncStorage.getItem('alreadyLaunched').then(value => {
-  //     if (value === null) {
-  //       AsyncStorage.setItem('alreadyLaunched', 'true');
-  //       setIsFirstLaunch(true);
-  //     } else {
-  //       setIsFirstLaunch(false);
-  //     }
-  //   });
-  //   GoogleSignin.configure({
-  //     webClientId:
-  //       '518009141888-dvllqk7rhar65r7f7prgokbn72f00ogl.apps.googleusercontent.com',
-  //   });
-  // }, []);
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value === null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+    GoogleSignin.configure({
+      webClientId:
+        '992403560552-qhe9jq6d9p01tlr0fp76etbqd7d58clm.apps.googleusercontent.com',
+    });
+  }, []);
 
-  // if (isFirstLaunch === null) {
-  //   return null; // This is the 'tricky' part: The query to AsyncStorage is not finished, but we have to present something to the user. Null will just render nothing, so you can also put a placeholder of some sort, but effectively the interval between the first mount and AsyncStorage retrieving your data won't be noticeable to the user. But if you want to display anything then you can use a LOADER here
-  // } else if (isFirstLaunch === true) {
-  //   routeName = 'Onboarding';
-  // } else {
-  //   routeName = 'Login';
-  // }
+  if (isFirstLaunch === null) {
+    return null; // This is the 'tricky' part: The query to AsyncStorage is not finished, but we have to present something to the user. Null will just render nothing, so you can also put a placeholder of some sort, but effectively the interval between the first mount and AsyncStorage retrieving your data won't be noticeable to the user. But if you want to display anything then you can use a LOADER here
+  } else if (isFirstLaunch === true) {
+    routeName = 'Onboarding';
+  } else {
+    routeName = 'Login';
+  }
 
   return (
     <Stack.Navigator initialRouteName={routeName}
