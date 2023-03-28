792b30fbc3dfddfd04c74a2dbd897dac0b996849
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-03-28 22:55:28 +0530

    Added shape names

--- App/components/shapesCard.js
+++ App/components/shapesCard.js
@@ -3,34 +3,53 @@ import {
   View,
   StyleSheet,
   Text,
-  TouchableHighlight,
-  ImageBackground,
+  Image,
 } from 'react-native';
 
-export default function ShapesCard({text, image, onPress}) {
+export default function ShapesCard({ text, image, onPress }) {
   return (
-    <ImageBackground
-      style={styles.card}
-      source={image}
-      imageStyle={{
-        width: 350,
-        height: 380,
-        borderRadius: 15,
-      }}
-    />
+    <View style={styles.card}>
+      <Image
+        source={image}
+        style={{
+          width: 300,
+          height: 260,
+          marginTop: 50,
+          borderRadius: 15,
+        }}
+      />
+      <View style={styles.card2}>
+        <Text style={styles.text2}>{text}</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text2: {
+    fontSize: 30,
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
   card: {
-    backgroundColor: '#dfedcb',
-    width: 350,
-    height: 380,
+    width: 360,
+    height: 450,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    margin: 20,
+    marginTop: 70,
+    backgroundColor: '#d29ffc',
     borderRadius: 15,
-    marginTop: 35,
+    elevation: 20,
+  },
+  card2: {
+    width: 300,
+    height: 100,
+    backgroundColor: '#fff',
+    borderRadius: 15,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 25,
-    elevation: 15,
   },
 });
