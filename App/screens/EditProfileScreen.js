22b487a5c76ab2759f7b74dfa4c1d4d8293ac570
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-04-07 20:26:07 +0530

    Removed profile picture section

--- App/screens/EditProfileScreen.js
+++ App/screens/EditProfileScreen.js
@@ -17,19 +17,11 @@ import Feather from 'react-native-vector-icons/Feather';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {windowHeight, windowWidth} from '../constants/Dimensions';
 
-// import Animated from 'react-native-reanimated';
-import BottomSheet from 'reanimated-bottom-sheet';
-import ImagePicker from 'react-native-image-crop-picker';
-
 import {AuthContext} from '../navigation/AuthProvider';
 import firestore from '@react-native-firebase/firestore';
-import storage from '@react-native-firebase/storage';
 
 const EditProfileScreen = ({navigation}) => {
   const {user, logout} = useContext(AuthContext);
-  const [image, setImage] = useState(null);
-  const [uploading, setUploading] = useState(false);
-  const [transferred, setTransferred] = useState(0);
   const [userData, setUserData] = useState(null);
 
   const getUser = async () => {
@@ -46,12 +38,6 @@ const EditProfileScreen = ({navigation}) => {
   };
 
   const handleUpdate = async () => {
-    let imgUrl = await uploadImage();
-
-    if (imgUrl == null && userData.userImg) {
-      imgUrl = userData.userImg;
-    }
-
     firestore()
       .collection('users')
       .doc(user.uid)
@@ -62,7 +48,6 @@ const EditProfileScreen = ({navigation}) => {
         phone: userData.phone,
         country: userData.country,
         city: userData.city,
-        userImg: imgUrl,
       })
       .then(() => {
         console.log('User Updated!');
@@ -73,189 +58,26 @@ const EditProfileScreen = ({navigation}) => {
       });
   };
 
-  const uploadImage = async () => {
-    if (image == null) {
-      return null;
-    }
-    const uploadUri = image;
-    let filename = uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
-
-    // Add timestamp to File Name
-    const extension = filename.split('.').pop();
-    const name = filename.split('.').slice(0, -1).join('.');
-    filename = name + Date.now() + '.' + extension;
-
-    setUploading(true);
-    setTransferred(0);
-
-    const storageRef = storage().ref(`photos/${filename}`);
-    const task = storageRef.putFile(uploadUri);
-
-    // Set transferred state
-    task.on('state_changed', taskSnapshot => {
-      console.log(
-        `${taskSnapshot.bytesTransferred} transferred out of ${taskSnapshot.totalBytes}`,
-      );
-
-      setTransferred(
-        Math.round(taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) *
-          100,
-      );
-    });
-
-    try {
-      await task;
-
-      const url = await storageRef.getDownloadURL();
-
-      setUploading(false);
-      setImage(null);
-
-      // Alert.alert(
-      //   'Image uploaded!',
-      //   'Your image has been uploaded to the Firebase Cloud Storage Successfully!',
-      // );
-      return url;
-    } catch (e) {
-      console.log(e);
-      return null;
-    }
-  };
-
-  useEffect(() => {
+    useEffect(() => {
     getUser();
   }, []);
 
-  const takePhotoFromCamera = () => {
-    ImagePicker.openCamera({
-      compressImageMaxWidth: 300,
-      compressImageMaxHeight: 300,
-      cropping: true,
-      compressImageQuality: 0.7,
-    }).then(image => {
-      console.log(image);
-      const imageUri = Platform.OS === 'ios' ? image.sourceURL : image.path;
-      setImage(imageUri);
-      this.bs.current.snapTo(1);
-    });
-  };
-
-  const choosePhotoFromLibrary = () => {
-    ImagePicker.openPicker({
-      width: 300,
-      height: 300,
-      cropping: true,
-      compressImageQuality: 0.7,
-    }).then(image => {
-      console.log(image);
-      const imageUri = Platform.OS === 'ios' ? image.sourceURL : image.path;
-      setImage(imageUri);
-      this.bs.current.snapTo(1);
-    });
-  };
-
-  renderInner = () => (
-    <View style={styles.panel}>
-      <View style={{alignItems: 'center'}}>
-        <Text style={styles.panelTitle}>Upload Photo</Text>
-        <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
-      </View>
-      <TouchableOpacity
-        style={styles.panelButton}
-        onPress={takePhotoFromCamera}>
-        <Text style={styles.panelButtonTitle}>Take Photo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.panelButton}
-        onPress={choosePhotoFromLibrary}>
-        <Text style={styles.panelButtonTitle}>Choose From Library</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.panelButton}
-        onPress={() => this.bs.current.snapTo(1)}>
-        <Text style={styles.panelButtonTitle}>Cancel</Text>
-      </TouchableOpacity>
-    </View>
-  );
-
-  renderHeader = () => (
-    <View style={styles.header}>
-      <View style={styles.panelHeader}>
-        <View style={styles.panelHandle} />
-      </View>
-    </View>
-  );
-
-  bs = React.createRef();
-  fall = new Animated.Value(1);
-
+  
   return (
     <ImageBackground
       source={require('../assets/images/bg.jpg')}
       style={styles.container}>
       <ScrollView>
-        {/* <BottomSheet
-          ref={this.bs}
-          snapPoints={[330, -5]}
-          renderContent={this.renderInner}
-          renderHeader={this.renderHeader}
-          initialSnap={1}
-          callbackNode={this.fall}
-          enabledGestureInteraction={true}
-        /> */}
         <Animated.View
           style={{
             margin: 20,
             opacity: Animated.add(0.1, Animated.multiply(this.fall, 1.0)),
           }}>
           <View style={{alignItems: 'center'}}>
-            <TouchableOpacity onPress={() => this.bs.current.snapTo(0)}>
-              <View
-                style={{
-                  height: 100,
-                  width: 100,
-                  borderRadius: 15,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <ImageBackground
-                  source={{
-                    uri: image
-                      ? image
-                      : userData
-                      ? userData.userImg ||
-                        'https://cdn2.vectorstock.com/i/thumb-large/04/71/person-icon-vector-2110471.jpg'
-                      : 'https://cdn2.vectorstock.com/i/thumb-large/04/71/person-icon-vector-2110471.jpg',
-                  }}
-                  style={{height: 100, width: 100}}
-                  imageStyle={{borderRadius: 100}}>
-                  <View
-                    style={{
-                      flex: 1,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}>
-                    <MaterialCommunityIcons
-                      name="camera"
-                      size={35}
-                      color="#333"
-                      style={{
-                        opacity: 0.7,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        borderWidth: 1,
-                        borderColor: '#333',
-                        borderRadius: 10,
-                      }}
-                    />
-                  </View>
-                </ImageBackground>
-              </View>
-            </TouchableOpacity>
             <Text style={{marginTop: 10, fontSize: 18, fontWeight: 'bold'}}>
               {userData ? userData.fname : ''} {userData ? userData.lname : ''}
             </Text>
-            <Text style={{marginBottom: 28}}>{user.uid}</Text>
+            <Text style={{marginBottom: 28}}>{userData ? userData.city : ''}, {userData ? userData.country : ''}</Text>
           </View>
 
           <View style={styles.action}>
