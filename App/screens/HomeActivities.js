aa94b1bca9e33f0647a4e06a2fa5c926723c34a3
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-04-08 22:19:02 +0530

    Added styles for activity button

--- App/screens/HomeActivities.js
+++ App/screens/HomeActivities.js
@@ -3,26 +3,46 @@ import {
   StyleSheet,
   View,
   Text,
-  SafeAreaView,
+  TouchableHighlight,
   ImageBackground,
+  ScrollView
 } from 'react-native';
-import ActivityButton from '../components/ActivityButton';
 
-const HomeActivities = ({navigation}) => {
+const HomeActivities = ({ navigation }) => {
   return (
     <ImageBackground
       source={require('../assets/images/bg.jpg')}
       style={styles.safearea}>
       <Text style={styles.heading}>Fun Activities</Text>
       <View style={styles.button_container}>
-        <ActivityButton
-          buttonTitle="සිංහල"
-          onPress={() => navigation.navigate('SinhalaActivityDrawer')}
-        />
-        <ActivityButton
-          buttonTitle="English"
-          onPress={() => navigation.navigate('ActivityScreenDrawer')}
-        />
+        <ScrollView horizontal={true}>
+        <ImageBackground
+          style={styles.card}
+          source={require('../assets/images/sinhalaActivity.jpg')}
+        >
+          <View>
+            <TouchableHighlight
+              underlayColor={'white'}
+              style={styles.button}
+              onPress={() => navigation.navigate('SinhalaActivityDrawer')}>
+              <Text style={styles.textArea_text}>සිංහල</Text>
+            </TouchableHighlight>
+          </View>
+        </ImageBackground>
+        <ImageBackground
+          style={styles.card}
+          source={require('../assets/images/english.jpg')}
+        >
+          <View>
+            <TouchableHighlight
+              underlayColor={'white'}
+              style={styles.button}
+              onPress={() => navigation.navigate('ActivityScreenDrawer')}>
+              <Text style={styles.textArea_text}>English</Text>
+            </TouchableHighlight>
+          </View>
+        </ImageBackground>
+        </ScrollView>
       </View>
     </ImageBackground>
   );
@@ -48,5 +68,39 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderTopLeftRadius: 200,
     borderTopRightRadius: 200,
+  },
+  textArea_text: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+  card: {
+    width: 400,
+    height: 350,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    marginBottom: 125,
+    marginLeft: 12,
+    elevation: 10,
+    width: 350,
+    height: 320,
+    borderRadius: 50,
+    marginLeft: 25,
+    marginTop: 40,
+    elevation: 10,
+  },
+  button: {
+    width: 350,
+    height: 65,
+    backgroundColor: '#c055e0',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    marginTop: 350,
+    elevation: 10,
   },
 });
