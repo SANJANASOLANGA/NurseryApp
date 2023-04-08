98da44d0639875ccdbb3522bc5fd7773c798ef97
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-04-08 19:41:53 +0530

    Changed card image

--- App/screens/sinhala/SinhalaPoemsScreen.js
+++ App/screens/sinhala/SinhalaPoemsScreen.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firestore from '@react-native-firebase/firestore';
+
 import {
+  FlatList,
   View,
   Text,
   StyleSheet,
@@ -7,56 +10,61 @@ import {
   ImageBackground,
   ScrollView,
 } from 'react-native';
-import PoemCard from '../../components/PoemCard';
+
 
 const SinhalaPoemsScreen = ({ navigation }) => {
+  const [poems, setPoems] = useState([]);
+
+  const getPoems = async () => {
+    const subscriber = firestore()
+      .collection('EnglishPoems')
+      .onSnapshot(querySnapshot => {
+        const poems = [];
+        console.log('querySnapshot' + querySnapshot);
+        querySnapshot.forEach(documentSnapshot => {
+          poems.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+
+        setPoems(poems);
+      });
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }
+
+  useEffect(() => {
+    getPoems()
+  }, []);
 
   return (
     <ImageBackground
       source={require('../../assets/images/bg.jpg')}
       style={styles.main}>
       <View style={styles.header}>
-        <Text style={styles.text}>Little Poems</Text>
+        <Text style={styles.text}>කවි ඉගෙන ගනිමු</Text>
       </View>
       <ScrollView>
         <View style={styles.body}>
-          <ScrollView horizontal={true}>
-            <PoemCard
-              text1={'The Crocodile'}
-              text2={
-                'How doth the little crocodile            Improve his shining tail,            And pour the waters of the Nile                       On every golden scale!              How cheerfully he seems to grin,            How neatly spreads his claws,            And welcomes little fishes in,            With gently smiling jaws!'
-              }
-              text3={'Lewis Carroll'}
-            />
-            <PoemCard
-              text1={'The Purple Cow'}
-              text2={
-                'I never saw a purple cow,               I never hope to see one,               But I can tell you, anyhow,            I’d rather see than be one!'
-              }
-              text3={'Gelett Burgess'}
-            />
-            <PoemCard
-              text1={'Hey Diddle Diddle'}
-              text2={
-                'Hey diddle diddle,                          The Cat and the fiddle,                 The Cow jumped over the moon,                                           The little Dog laughed to see such sport,                                    And the Dish ran away with the Spoon.'
-              }
-              text3={'Author Unknown'}
-            />
-            <PoemCard
-              text1={'Twinkle, Twinkle, Little Star'}
-              text2={
-                'Twinkle, twinkle, little star,            How I wonder what you are.                      Up above the world so high,            Like a diamond in the sky.            Twinkle, twinkle, little star,            How I wonder what you are!'
-              }
-              text3={'Jane Taylor'}
-            />
-            <PoemCard
-              text1={'Star Light, Star Bright'}
-              text2={
-                'Star light, start bright,                The first star I see tonight;             I wish I may, I wish I might,            Have the wish I wish tonight.'
-              }
-              text3={'Author Unknown'}
-            />
-          </ScrollView>
+          <FlatList
+            horizontal={true}
+            data={poems}
+            renderItem={({ item }) => (
+              <ImageBackground
+                source={require('../../assets/images/poems.png')}
+                imageStyle={{ borderRadius: 15, width: 375, height: 500 }}
+                style={styles.card}
+              >
+                <Text style={styles.text1}>{item.topic}</Text>
+                <View style={styles.card2}>
+                  <Text style={styles.text2}>{item.body}</Text>
+                </View>
+                <Text style={styles.text3}>{item.author}</Text>
+              </ImageBackground>
+            )}
+          />
         </View>
         <View style={styles.bottom}>
           <TouchableOpacity
@@ -83,7 +91,6 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   header: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     borderBottomLeftRadius: 100,
@@ -101,7 +108,7 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 33,
-    marginTop: 10,
+    marginTop: '5%',
     textAlign: 'center',
     marginBottom: 20,
   },
@@ -118,5 +125,51 @@ const styles = StyleSheet.create({
   btn_txt: {
     fontSize: 20,
     color: 'white'
+  },
+  text1: {
+    fontSize: 30,
+    marginTop: 70,
+    fontWeight: '500',
+    color: '#54225e'
+  },
+  text2: {
+    fontSize: 26,
+    textAlign: 'left',
+    marginLeft: 15,
+    marginTop: 15,
+    fontStyle: 'italic',
+    fontWeight: '300',
+  },
+  text3: {
+    fontSize: 20,
+    textAlign: 'right',
+    textAlignVertical: 'bottom',
+    marginLeft: 150,
+    fontStyle: 'italic',
+    marginTop: 60,
+    color: '#300240',
+    fontWeight: '500',
+  },
+  card: {
+    width: 375,
+    height: 500,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    margin: 25,
+    backgroundColor: '#eecdf7',
+    borderRadius: 15,
+    elevation: 10,
+  },
+  card2: {
+    width: 335,
+    height: 270,
+    backgroundColor: '#eecdf7',
+    borderRadius: 15,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 15,
   },
 });
