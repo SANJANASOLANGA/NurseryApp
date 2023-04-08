a773441494b43804bfed8a27efbd383780e9a0cb
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-04-08 21:54:36 +0530

    Changed height of button

--- App/components/ActivityButton.js
+++ App/components/ActivityButton.js
@@ -16,9 +16,9 @@ const styles = StyleSheet.create({
   buttonContainer: {
     marginTop: 25,
     width: '75%',
-    height: windowHeight / 11,
+    height: windowHeight / 9,
     backgroundColor: '#8a36d1',
-    padding: 20,
+    padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 20,
